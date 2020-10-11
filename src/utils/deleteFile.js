de77da501304253718c07d5966e27579e31237ab
Alexander Augusto <alexaasf_10@hotmail.com>
2020-10-11 13:06:24 -0300

    delete file from cloudinary

--- src/utils/deleteFile.js
+++ src/utils/deleteFile.js
@@ -1,15 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 
+const cloudinary = require('../config/cloudinary')
+
 module.exports = (file) => {
-  const pathname = path.resolve(__dirname, '../../tmp/uploads/' + file)
-  if (!pathname)
-    return
-    
-  fs.unlink(pathname, (err) => {
-    if (err) {
-      console.error(err)
+  if (process.env.STORAGE_TYPE === 'local') {
+    const pathname = path.resolve(__dirname, '../../tmp/uploads/' + file)
+    if (!pathname)
       return
-    }
-  })
+
+    fs.unlink(pathname, (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  }else{
+    cloudinary.uploader.destroy(file.split('.').slice(0, -1).join('.'), (err, result) => console.log(err, result))
+  }
 }
