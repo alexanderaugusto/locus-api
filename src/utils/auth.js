c5132264e45ea2b04093e4fde491c0c731fadc67
Alexander Augusto <alexaasf_10@hotmail.com>
2020-11-01 11:19:44 -0300

    create a default AUTH_SECRET

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,13 +10,13 @@ const comparePassword = (password1, password2) => {
 }
 
 const generateJwt = (params) => {
-  return jwt.sign(params, process.env.AUTH_SECRET || "default-secret", {
+  return jwt.sign(params, process.env.AUTH_SECRET || "268e40a6c231f8e4a907493447d0c72a", {
     expiresIn: 172800
   })
 }
 
 const verifyJwt = (token, cb) => {
-  return jwt.verify(token, process.env.AUTH_SECRET || "default-secret", cb)
+  return jwt.verify(token, process.env.AUTH_SECRET || "268e40a6c231f8e4a907493447d0c72a", cb)
 }
 
 module.exports = {
