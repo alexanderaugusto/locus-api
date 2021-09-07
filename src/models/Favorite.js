a476a6fa57d4271b1334c11a3414e496c5b2dab6
Alexander Augusto <alexaasf_10@hotmail.com>
2021-09-07 08:34:15 -0300

    add id in favorite and rental model

--- src/models/Favorite.js
+++ src/models/Favorite.js
@@ -1,6 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Favorite = sequelize.define('Favorite', {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
   })
 
   return Favorite
--- src/models/Rental.js
+++ src/models/Rental.js
@@ -1,6 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Rental = sequelize.define('Rental', {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
   })
 
   return Rental
